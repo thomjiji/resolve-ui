dc8cda260b7b5d90b744f1214aa995c4dfb77ab2
thomjiji <hxy449999@gmail.com>
2022-10-01 23:24:07 +0800

    show path initially works fine

--- src/sample.py
+++ src/sample.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from turtle import numinput
 from pybmd import Bmd
 from pybmd import toolkits
@@ -7,14 +9,14 @@ project = resolve.get_project_manager().get_current_project()
 media_pool = project.get_media_pool()
 root_folder = media_pool.get_root_folder()
 
-
 ui = fusion.UIManager
 dispatcher = bmd.UIDispatcher(ui)
 
-
 createBinID = "Create bin"
-inputID = "Input Field"
+inputID = "Input field"
 testClickID = "Test click"
+pathTreeID = "Path tree"
+showPathID = "Show path"
 
 # Define the window UI layout
 win = dispatcher.AddWindow(
@@ -48,16 +50,27 @@ win = dispatcher.AddWindow(
                     "Weight": 0,
                 },
                 [
-                    ui.Button({"ID": createBinID, "Text": "Create bin", "Weight": 0}),
+                    ui.Button(
+                        {
+                            "ID": createBinID,
+                            "Text": "Create bin",
+                            "Weight": 0,
+                        },
+                    ),
                     ui.Button(
                         {
                             "ID": testClickID,
                             "Text": "Test Click",
                             "Weight": 0,
-                            "Height": 0,
                         }
                     ),
-                    ui.Button({"Text": "Replace", "Weight": 0}),
+                    ui.Button(
+                        {
+                            "ID": showPathID,
+                            "Text": "Show Path",
+                            "Weight": 0,
+                        },
+                    ),
                 ],
             ),
             ui.LineEdit(
@@ -76,6 +89,7 @@ win = dispatcher.AddWindow(
             ),
             ui.Tree(
                 {
+                    "ID": pathTreeID,
                     "AlternatingRowColors": True,
                     "HeaderHidden": True,
                     "SelectionMode": "ExtendedSelection",
@@ -87,6 +101,7 @@ win = dispatcher.AddWindow(
 )
 
 itm = win.GetItems()
+
 
 # Define the events handlers
 def on_close(ev):
@@ -102,10 +117,19 @@ def test_click(ev):
     print(f"{itm[inputID].Text}")
 
 
+def show_tree(ev):
+    top_level_items = []
+    row = itm[pathTreeID].NewItem()
+    row.Text[0] = itm[inputID].Text
+    top_level_items.append(row)
+    itm[pathTreeID].AddTopLevelItems(top_level_items)
+
+
 # Assign events handlers
 win.On.myWindow.Close = on_close
 win.On[createBinID].Clicked = on_create_bin
 win.On[testClickID].Clicked = test_click
+win.On[showPathID].Clicked = show_tree
 
 
 if __name__ == "__main__":
