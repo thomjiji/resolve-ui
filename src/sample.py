0b7040263e570bda02d7809905d249baebe90610
thomjiji <hxy449999@gmail.com>
2022-10-01 20:03:22 +0800

    Button event: create bin works

--- src/sample.py
+++ src/sample.py
@@ -1,8 +1,20 @@
+from turtle import numinput
+from pybmd import Bmd
+from pybmd import toolkits
+
+resolve = Bmd()
+project = resolve.get_project_manager().get_current_project()
+media_pool = project.get_media_pool()
+root_folder = media_pool.get_root_folder()
+
+
 ui = fusion.UIManager
 dispatcher = bmd.UIDispatcher(ui)
 
 
-ClearID = 'Clear'
+createBinID = "Create bin"
+inputID = "Input Field"
+testClickID = "Test click"
 
 # Define the window UI layout
 win = dispatcher.AddWindow(
@@ -22,14 +34,10 @@ win = dispatcher.AddWindow(
                     "Weight": 0,
                 },
                 [
-                    ui.LineEdit({"PlaceholderText": "Text Field"}),
-                    ui.LineEdit({"PlaceholderText": "Text Field"}),
-                    ui.SpinBox(
+                    ui.LineEdit(
                         {
-                            "Value": 2,
-                            "Minimum": 1,
-                            "Maximum": 99,
-                            "SingleStep": 1,
+                            "ID": inputID,
+                            "PlaceholderText": "Text Field",
                         }
                     ),
                 ],
@@ -40,14 +48,30 @@ win = dispatcher.AddWindow(
                     "Weight": 0,
                 },
                 [
-                    ui.Button({"ID": ClearID,"Text": "Clear", "Weight": 0}),
-                    ui.Button({"Text": "Find", "Weight": 0, "Height": 0}),
+                    ui.Button({"ID": createBinID, "Text": "Create bin", "Weight": 0}),
+                    ui.Button(
+                        {
+                            "ID": testClickID,
+                            "Text": "Test Click",
+                            "Weight": 0,
+                            "Height": 0,
+                        }
+                    ),
                     ui.Button({"Text": "Replace", "Weight": 0}),
                 ],
             ),
             ui.LineEdit(
                 {
                     "PlaceholderText": "example text",
+                }
+            ),
+            ui.SpinBox(
+                {
+                    "Value": 2,
+                    "Minimum": 1,
+                    "Maximum": 99,
+                    "SingleStep": 1,
+                    "Weight": 0,
                 }
             ),
             ui.Tree(
@@ -62,17 +86,26 @@ win = dispatcher.AddWindow(
     ),
 )
 
+itm = win.GetItems()
+
 # Define the events handlers
-def OnClose(ev):
+def on_close(ev):
     dispatcher.ExitLoop()
 
-def OnClear(ev):
-    print("Clear button was pressed once!")
-    
+
+def on_create_bin(ev):
+    path = itm[inputID].Text
+    toolkits.add_subfolders(media_pool, root_folder, path)
+
+
+def test_click(ev):
+    print(f"{itm[inputID].Text}")
+
 
 # Assign events handlers
-win.On.myWindow.Close = OnClose
-win.On[ClearID].Clicked = OnClear
+win.On.myWindow.Close = on_close
+win.On[createBinID].Clicked = on_create_bin
+win.On[testClickID].Clicked = test_click
 
 
 if __name__ == "__main__":
